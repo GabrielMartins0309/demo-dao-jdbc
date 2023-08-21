7d1a7436c3630a4339bb59eefa660f575ba8454f
Gabriel Martins Rodrigues <gabriel.rodrigues.01@edu.ufes.br>
2023-08-21 00:55:43 -0300

    Criado as classes SellerDaoJDBC e DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import entities.model.Department;
+import entities.model.Seller;
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
